82cad91788b5247891ec383ac31817fcbfbb4e24
Saadkhan0105 <saadkhan9560@gmail.com>
2025-09-22 18:22:43 +0530

    Datatypes in Python

---
+++ Python/04_datatypes/datatypes.py
@@ -0,0 +1,15 @@
+age = 3
+print(age)
+print(type(age))
+
+cgpa = 8.2
+print(cgpa)
+print(type(cgpa))
+
+name = "Saad"
+print(name)
+print(type(name))
+
+is_completed = True
+print(is_completed)
+print(type(is_completed))
