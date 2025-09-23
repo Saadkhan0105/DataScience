c566bfe8d97f4752210f314303c57fc7dedc33b5
Saadkhan0105 <saadkhan9560@gmail.com>
2025-09-23 18:35:54 +0530

    Taking user input

--- Python/06_user_input/user_input.py
+++ Python/06_user_input/user_input.py
@@ -1,4 +1,4 @@
-'''
+# Taking user input in Python
 a = input("Enter a number: ")
 print("You entered:", a)
 
@@ -9,10 +9,10 @@ print("You entered:", b)
 # Program to add two numbers
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
-# Adding two numbers (Note: input() returns strings, so we need to convert them to integers)
+
 sum = num1 + num2
 print("The sum is:", sum)
-''' 
+
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 print(f"Hello {name}, you are {age} is years old! ")
