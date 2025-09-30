c04057a0fa9e19277ff446932387852a68167e4d
Saadkhan0105 <saadkhan9560@gmail.com>
2025-09-30 22:24:26 +0530

    Slicing

--- Python/11_strings/slicing.py
+++ Python/11_strings/slicing.py
@@ -1,6 +1,10 @@
-name = "Saad"
+name = "Saad0123456789"
 print(name[0:2])  
 print(name[:3])   
 print(name[1:])   
 print(name[-1])   
 print(name[-3:-1]) 
+
+print(name[0:10:2]) # print every 2nd character from index 0 to 9
+print(name[::3])    # print every 3rd character from the whole string
+print(name[::-1])   # print the string in reverse order
