cc1ed02f06b4fcae05300373dd4cfbe240bedde4
Saadkhan0105 <saadkhan9560@gmail.com>
2025-10-10 16:49:03 +0530

    OS modules

---
+++ Python/19_Files/os.py
@@ -0,0 +1,9 @@
+import os
+import shutil
+
+a = os.listdir("dir")
+print(a)
+print(os.getcwd())
+print(os.path.exists("Saad"))
+# os.remove("sample.txt")
+os.rmdir("dir")
---
+++ Python/19_Files/shutil.py
@@ -0,0 +1,5 @@
+import shutil
+
+shutil.rmtree("dir")
+# shutil.copy("sample.txt", "copy.txt")
+# shutil.move("copy.txt", "dir")
